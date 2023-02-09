f1dea76b1215419c852a62336169f404086dd81e
pierre-agana <pierre.agana@gmail.com>
2023-02-09 15:14:54 +0000

    create Exception commit

---
+++ src/main/java/com/agana/gestiondestock/exception/ErrorCodes.java
@@ -0,0 +1,34 @@
+package com.agana.gestiondestock.exception;
+
+public enum ErrorCodes {
+  ARTICLE_NOT_FOUND(1000),
+  ARTICLE_NOT_VALID(1001),
+  CATEGORY_NOT_FOUND(2000),
+  CATEGORY_NOT_VALID(2001),
+  // TODO complete the rest of the Error codes
+  CUSTOMER_NOT_FOUND(3000),
+  CUSTOMER_NOT_VALID(3001),
+  ORDER_CUSTOMER_NOT_FOUND(4000),
+  ORDER_PROVIDER_NOT_FOUND(5000),
+  COMPANIES_NOT_FOUND(6000),
+  COMPANIES_NOT_VALID(6001),
+  PROVIDER_NOT_FOUND(7000),
+  PROVIDER_NOT_VALID(7001),
+  LINE_ORDER_CUSTOMER_NOT_FOUND(8000),
+  LINE_ORDER_PROVIDER_NOT_FOUND(9000),
+  LINE_SALE_NOT_FOUND(10000),
+  MVT_STK_NOT_FOUND(11000),
+  USER_NOT_FOUND(12000),
+  USER_NOT_VALID(12001),
+  SALE_NOT_FOUND(13000);
+
+
+  private int code;
+  ErrorCodes(int code) {
+    this.code = code;
+  }
+
+  public int getCode() {
+    return code;
+  }
+}
